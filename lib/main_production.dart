74d112c148971e5b01307c4a9fc84430d83e7d49
OtakaEmara <osama7emara@gmail.cm>
2025-07-31 15:44:59 +0300

    Firebase setup

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,11 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:osmer/firebase_options.dart';
 import 'package:osmer/my_app.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await ScreenUtil.ensureScreenSize();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
